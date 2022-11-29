e5b1dec6874af505807ac27df07725fc6fc8ddeb
Stayin_blick <azeezbello75@gmail.com>
2022-11-29 16:05:18 +0000

    updates worksheet

--- run.py
+++ run.py
@@ -15,6 +15,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     get sales figures input from the user
+    run a while loop to collect a valid string of data from the user
+    via the terminal which must be a string of 6 number separated 
+    by commas, the loop will repeatedly request data until its valid
     """ 
     while True:
         print("please enter sales data from the last market")
@@ -24,7 +27,6 @@ def get_sales_data():
         data_str = input("enter your data here: ")
 
         sales_data = data_str.split(",")
-        
 
         if validate_data (sales_data):
             print("data is valid")
@@ -49,4 +51,16 @@ def validate_data (values):
 
     return True  
 
+def update_sales_worksheet (data):
+    """ 
+    update salesheet, add new row with the list data provided
+    """
+    print ("updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales worksheet upadted succesfully \n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
