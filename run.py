9c2d7e3927c7152466cc4958592bd103e5eae4d3
Stayin_blick <azeezbello75@gmail.com>
2022-11-29 20:08:41 +0000

    get last 5 enteries sales in list of colunns

--- run.py
+++ run.py
@@ -82,6 +82,22 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_enteries_sales ():
+    """
+    collects collumns of data from sales worksheet, collecting
+    the last 5 enteries for each sandwhich and return data as 
+    a list of list
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+        return columns
+
+
 def main():
     """
     run all program functions
@@ -94,4 +110,5 @@ def main():
     update_worksheet(new_surplus_data, "surplus")    
 
 print("welcome to love snadwhiches data automation")
-main()
+#main()
+sales_columns = get_last_5_enteries_sales()
